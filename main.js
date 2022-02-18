c2cdfb0afd919d5865e84d14e4f312355af1bcbf
bonggoilabo <97515351+bonggoilabo@users.noreply.github.com>
2022-02-18 08:05:07 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('.slider-main').slick({
+        slidesToShow: 2.1,
+        slidesToScroll: 2,
+        infinite: false,
+        dots: true,
+        prevArrow:
+           `<button type='button' class='slick-prev pull-left'><i class="fas fa-chevron-left slick-prev"></i></button>`,
+        nextArrow:
+           `<button type='button' class='slick-next pull-right'><i class="fas fa-chevron-right slick-next"></i></button>`
+    
+    });
+  });
+
+$(document).ready(function(){
+    $('.content1__slider-main').slick({
+       slidesToShow: 6.5,
+       slidesToScroll: 6,
+       infinite: false,
+       dots: true,
+       prevArrow:
+          `<button type='button' class='slick-prev pull-left'><i class="fas fa-chevron-left slick-prev"></i></button>`,
+       nextArrow:
+          `<button type='button' class='slick-next pull-right'><i class="fas fa-chevron-right slick-next"></i></button>`
+   
+    });
+ });
+ $(document).ready(function(){
+   $('.content1__slider-main').slick({
+      slidesToShow: 6.5,
+      slidesToScroll: 6,
+      infinite: false,
+      dots: true,
+      prevArrow:
+         `<button type='button' class='slick-prev pull-left'><i class="fas fa-chevron-left slick-prev"></i></button>`,
+      nextArrow:
+         `<button type='button' class='slick-next pull-right'><i class="fas fa-chevron-right slick-next"></i></button>`
+  
+   });
+});
